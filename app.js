da85e0ba4cb0b2e458cab39d6a247f576c4c860b
victor Macharia <Techvictor653@gmail.com>
2023-05-20 14:49:21 +0300

    continuing with cool portfolio

--- app.js
+++ app.js
@@ -6,17 +6,39 @@ const allSelections = document.querySelectorAll('.main-content')
 function pageTransitions() {
     //button clock active class
     for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function(event){
+        sectBtn[i].addEventListener('click', function (event) {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             // this.className += 'active-btn';(very very stubborn to use had to ask chatGPT for the answer below
+            //className replaces the whole class, classList can set attributes to a specific class.
             event.target.classList.add('active-btn');
         })
     }
+
     //sections active class
-    allSelections.addEventListener('click', function(e){
-        console.log(e.target);
+    allSelections.forEach(function (element) {
+        element.addEventListener('click', function (event) {
+            //console.log(event.target);
+            /*addEventListener can only be applied to individual elements.Not nodelists*/
+            const id = event.target.dataset.id;
+            if (id) {
+                //reamove selected from the other buttons
+                sectBtn.forEach(function (btn) {
+                    btn.classList.remove('active');
+                })
+                event.target.classList.add('active');
+
+                //hide the other sections
+                sections.forEach(function (section) {
+                    section.classList.remove('active');
+                })
+
+                const element = document.getElementById(id);
+                element.classList.add('active');
+            }
+        })
     })
+
 }
 pageTransitions();
 
